e594ebffea8e9b15bfe5e811abc09dc8d7bae655
Eivind Morch <eivind@morch.priv.no>
2022-05-12 13:41:03 +0200

    Add publishing of error events to kafka

--- src/main/java/no/fintlabs/InstanceProcessingErrorHandlerService.java
+++ src/main/java/no/fintlabs/InstanceProcessingErrorHandlerService.java
@@ -1,54 +1,28 @@
 package no.fintlabs;
 
 import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.flyt.kafka.InstanceFlowErrorHandler;
+import no.fintlabs.flyt.kafka.headers.InstanceFlowHeaders;
+import no.fintlabs.flyt.kafka.headers.InstanceFlowHeadersMapper;
 import no.fintlabs.integration.error.ErrorEventProducerService;
-import org.springframework.kafka.listener.CommonLoggingErrorHandler;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class InstanceProcessingErrorHandlerService extends CommonLoggingErrorHandler {
-
-    // TODO: 28/03/2022 Inlcude skjema headers in outgoing error events
+public class InstanceProcessingErrorHandlerService extends InstanceFlowErrorHandler {
 
     private final ErrorEventProducerService errorEventProducerService;
 
-    public InstanceProcessingErrorHandlerService(ErrorEventProducerService errorEventProducerService) {
+    public InstanceProcessingErrorHandlerService(InstanceFlowHeadersMapper instanceFlowHeadersMapper, ErrorEventProducerService errorEventProducerService) {
+        super(instanceFlowHeadersMapper);
         this.errorEventProducerService = errorEventProducerService;
     }
 
-//    @Override
-//    public void handleOtherException(Exception thrownException, Consumer<?, ?> consumer, MessageListenerContainer container, boolean batchListener) {
-//        super.handleOtherException(thrownException, consumer, container, batchListener);
-//        sendErrorEvent(thrownException);
-//    }
-//
-//    @Override
-//    public void handleRecord(Exception thrownException, ConsumerRecord<?, ?> record, Consumer<?, ?> consumer, MessageListenerContainer container) {
-//        super.handleRecord(thrownException, record, consumer, container);
-//        sendErrorEvent(thrownException);
-//    }
-//
-//    @Override
-//    public void handleBatch(Exception thrownException, ConsumerRecords<?, ?> data, Consumer<?, ?> consumer, MessageListenerContainer container, Runnable invokeListener) {
-//        super.handleBatch(thrownException, data, consumer, container, invokeListener);
-//        sendErrorEvent(thrownException);
-//    }
-//
-//    private void sendErrorEvent(Exception thrownException) {
-//        if (thrownException instanceof MissingInstanceFieldsValidationException) {
-//            errorEventProducerService.sendMissingInstanceFieldsErrorEvent(
-//                    (MissingInstanceFieldsValidationException) thrownException
-//            );
-//        } else if (thrownException instanceof MissingMappingFieldsValidationException) {
-//            errorEventProducerService.sendMissingMappingFieldsErrorEvent(
-//                    (MissingMappingFieldsValidationException) thrownException
-//            );
-//        } else if (thrownException instanceof NoSuchIntegrationConfigurationException) {
-//            errorEventProducerService.sendNoConfigurationForIntegrationErrorEvent();
-//        } else {
-//            errorEventProducerService.sendGeneralSystemErrorEvent();
-//        }
-//    }
+    @Override
+    public void handleInstanceFlowRecord(Exception thrownException, InstanceFlowHeaders instanceFlowHeaders, ConsumerRecord<?, ?> consumerRecord) {
+        errorEventProducerService.sendGeneralSystemErrorEvent(instanceFlowHeaders);
+        log.error("Could not process instance record", thrownException);
+    }
 
 }
--- src/main/java/no/fintlabs/InstanceProcessingService.java
+++ src/main/java/no/fintlabs/InstanceProcessingService.java
@@ -6,8 +6,11 @@ import no.fintlabs.flyt.kafka.InstanceFlowConsumerRecord;
 import no.fintlabs.flyt.kafka.headers.InstanceFlowHeaders;
 import no.fintlabs.integration.CaseEventProducerService;
 import no.fintlabs.integration.SkjemaConfigurationRequestService;
+import no.fintlabs.integration.error.ErrorEventProducerService;
 import no.fintlabs.model.configuration.IntegrationConfiguration;
 import no.fintlabs.model.instance.Instance;
+import no.fintlabs.validation.exceptions.MissingInstanceFieldsValidationException;
+import no.fintlabs.validation.exceptions.MissingMappingFieldsValidationException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,21 +20,47 @@ public class InstanceProcessingService {
     private final SkjemaConfigurationRequestService skjemaConfigurationRequestService;
     private final CaseService caseService;
     private final CaseEventProducerService caseEventProducerService;
+    private final ErrorEventProducerService errorEventProducerService;
 
     public InstanceProcessingService(
             SkjemaConfigurationRequestService skjemaConfigurationRequestService,
             CaseService caseService,
-            CaseEventProducerService caseEventProducerService
-    ) {
+            CaseEventProducerService caseEventProducerService,
+            ErrorEventProducerService errorEventProducerService) {
         this.skjemaConfigurationRequestService = skjemaConfigurationRequestService;
         this.caseService = caseService;
         this.caseEventProducerService = caseEventProducerService;
+        this.errorEventProducerService = errorEventProducerService;
     }
 
     public void process(InstanceFlowConsumerRecord<Instance> flytConsumerRecord) {
+        try {
+            mapToAndPublishCase(flytConsumerRecord);
+        } catch (MissingInstanceFieldsValidationException e) {
+            errorEventProducerService.sendMissingInstanceFieldsErrorEvent(
+                    flytConsumerRecord.getInstanceFlowHeaders(),
+                    e
+            );
+        } catch (MissingMappingFieldsValidationException e) {
+            errorEventProducerService.sendMissingMappingFieldsErrorEvent(
+                    flytConsumerRecord.getInstanceFlowHeaders(),
+                    e
+            );
+        } catch (NoSuchIntegrationConfigurationException e) {
+            errorEventProducerService.sendNoConfigurationForIntegrationErrorEvent(
+                    flytConsumerRecord.getInstanceFlowHeaders()
+            );
+        } catch (RuntimeException e) {
+            errorEventProducerService.sendGeneralSystemErrorEvent(
+                    flytConsumerRecord.getInstanceFlowHeaders()
+            );
+        }
+    }
+
+    private void mapToAndPublishCase(InstanceFlowConsumerRecord<Instance> flytConsumerRecord) {
         InstanceFlowHeaders consumerRecordInstanceFlowHeaders = flytConsumerRecord.getInstanceFlowHeaders();
 
-        String integrationId =  consumerRecordInstanceFlowHeaders.getIntegrationId();
+        String integrationId = consumerRecordInstanceFlowHeaders.getIntegrationId();
         IntegrationConfiguration integrationConfiguration = this.skjemaConfigurationRequestService.get(integrationId)
                 .orElseThrow(() -> new NoSuchIntegrationConfigurationException(integrationId));
 
