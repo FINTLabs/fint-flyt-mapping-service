051ffbf7253505939711fc3384e86ed350771e00
Eivind Morch <eivind@morch.priv.no>
2022-10-13 18:01:10 +0200

    FST-136 #comment Change to less strict regex

--- src/main/java/no/fintlabs/mapping/DynamicStringMappingService.java
+++ src/main/java/no/fintlabs/mapping/DynamicStringMappingService.java
@@ -13,8 +13,7 @@ import java.util.regex.Pattern;
 @Service
 public class DynamicStringMappingService extends DynamicInstanceFieldMapper {
 
-    private static final Pattern allowedCharacters = Pattern.compile("[^${}]");
-    private static final Pattern ifReferencePattern = Pattern.compile("\\$if\\{" + allowedCharacters.pattern() + "+}");
+    private static final Pattern ifReferencePattern = Pattern.compile("\\$if\\{[^}]+}");
 
     @Override
     protected MappedInstanceField.Type getMappedInstanceFieldType() {
