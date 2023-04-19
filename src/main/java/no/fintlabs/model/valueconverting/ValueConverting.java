951d3c9a48b0ca5cad2491c59fcd16b20855fccb
Eivind Morch <eivind@morch.priv.no>
2023-04-19 18:31:42 +0200

    FF-713 #comment Add temporary support for value converting

---
+++ src/main/java/no/fintlabs/model/valueconverting/ValueConverting.java
@@ -0,0 +1,19 @@
+package no.fintlabs.model.valueconverting;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.Map;
+
+@Getter
+@Builder
+@Jacksonized
+public class ValueConverting {
+    private final Long id;
+    private final String fromApplicationId;
+    private final String fromTypeId;
+    private final String toApplicationId;
+    private final String toTypeId;
+    private final Map<String, String> convertingMap;
+}
