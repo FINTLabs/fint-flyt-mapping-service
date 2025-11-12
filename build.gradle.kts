44d8d19b2c92f4502039a16e1ecf9c9a5275d18e
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-11-12 09:53:30 +0100

    Change from Groovy to Kotlin style Gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("java")
+    id("com.github.ben-manes.versions") version "0.52.0"
+}
+
+group = "no.novari"
+version = "0.0.1-SNAPSHOT"
+
+val apiVersion: String by project
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.named("annotationProcessor").get())
+    }
+}
+
+tasks.jar {
+    isEnabled = false
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.fintlabs.no/releases")
+    }
+    mavenLocal()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("no.fint:fint-arkiv-resource-model-java:$apiVersion")
+    implementation("org.apache.commons:commons-lang3:3.18.0")
+
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("no.novari:flyt-resource-server:6.0.0-rc-20")
+    implementation("no.novari:kafka:5.0.0-rc-16")
+    implementation("no.novari:flyt-kafka:4.0.0-rc-4")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
