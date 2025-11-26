a943b4a2918dbd7a7f011afeecfd5f5746381ab2
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2025-11-26 08:24:29 +0100

    Bump latest RC of resource-server, kafka, flyt-kafka, and fint-model

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-    implementation("org.springframework.kafka:spring-kafka")
+    implementation("no.fint:fint-arkiv-resource-model-java:$fintModelVersion")
+    implementation("no.novari:flyt-resource-server:6.0.0-rc-27")
+    implementation("no.novari:kafka:5.0.0-rc-21")
+    implementation("no.novari:flyt-kafka:4.0.0-rc-10")
 
     implementation("org.apache.commons:commons-lang3:3.18.0")
 
@@ -40,11 +43,6 @@ dependencies {
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-
-    implementation("no.fint:fint-arkiv-resource-model-java:$fintModelVersion")
-    implementation("no.novari:flyt-resource-server:6.0.0-rc-26")
-    implementation("no.novari:kafka:5.0.0-rc-20")
-    implementation("no.novari:flyt-kafka:4.0.0-rc-8")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
