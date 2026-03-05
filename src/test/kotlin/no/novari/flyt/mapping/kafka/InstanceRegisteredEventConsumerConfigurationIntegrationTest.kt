3b471241dc19d0fb4c8581ed84ac8a49c114bc30
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-03-05 15:38:58 +0100

    refactor to kotlin, bump java, spring boot and webflux to web

--- src/test/kotlin/no/novari/flyt/mapping/kafka/InstanceRegisteredEventConsumerConfigurationIntegrationTest.kt
+++ src/test/kotlin/no/novari/flyt/mapping/kafka/InstanceRegisteredEventConsumerConfigurationIntegrationTest.kt
@@ -24,9 +24,9 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.eq
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
